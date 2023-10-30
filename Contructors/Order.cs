8ffbff89aedd0624b6c9d4a7c26dc727069c2489
SarveshKadam-NimapInfotech <Sarveshkadam@nimapinfotech.com>
2023-10-30 19:10:30 +0530

    Classes, Constructors and Constructor Overloading Task

---
+++ Contructors/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace Contructors
+{
+    public class Order
+    {
+        public int OrderId;
+        public string ProductName;
+
+        public Order(int id, string name)
+        {
+            this.OrderId = id;
+            this.ProductName = name;
+
+
+        }
+    }
+}
---
+++ Contructors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contructors
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            var order = new Order(1, "laptop");
+             var order1= new Order(2, "book");
+
+            customer.Id = 1;
+            customer.Name = "Sarvesh";
+            customer.Orders.Add(order);
+            customer.Orders.Add(order1);
+
+            foreach(var item in customer.Orders)
+            {
+                Console.WriteLine(item.OrderId);
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+
+        }
+    }
+}
